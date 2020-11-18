fc14cd400da1abe49e183b6fbf9f93827a3d6b85
KoheiMoroi <koheimoroi@Koheis-MacBook-Pro.local>
2020-11-18 13:55:41 +0900

    add validation extenstion - add unit test for validation class

---
+++ app/src/test/java/app/atta/localize/ValidationUnitTest.kt
@@ -0,0 +1,63 @@
+package app.atta.localize
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import java.util.*
+
+
+class ValidationUnitTest {
+
+    @Test
+    fun `validate postal code in japan`() {
+        val input1 = "123-0001"
+        val input2 = "12-0001"
+        val input3 = "1-0001"
+        val input4 = "1230001"
+        val input5 = "0001"
+        val input6 = "aaaa"
+        val locale = Locale.JAPAN
+        assertTrue(input1.isValidPostalCode(locale))
+        assertFalse(input2.isValidPostalCode(locale))
+        assertFalse(input3.isValidPostalCode(locale))
+        assertFalse(input4.isValidPostalCode(locale))
+        assertFalse(input5.isValidPostalCode(locale))
+        assertFalse(input6.isValidPostalCode(locale))
+    }
+
+    @Test
+    fun `validate phone number in japan`() {
+        val input1 = "09000010004"
+        val input2 = "090-0001-0002"
+        val input3 = "+81 9000010004"
+        val input4 = "+81 90-0001-0002"
+        val input5 = "0-0001-0002"
+        val input6 = "000010002"
+        val input7 = "03-0001-0002"
+        val input8 = "aaaa"
+        val locale = Locale.JAPAN
+
+        assertTrue(input1.isValidPhoneNumber(locale))
+        assertTrue(input2.isValidPhoneNumber(locale))
+        assertTrue(input3.isValidPhoneNumber(locale))
+        assertTrue(input4.isValidPhoneNumber(locale))
+        assertFalse(input5.isValidPhoneNumber(locale))
+        assertFalse(input6.isValidPhoneNumber(locale))
+        assertTrue(input7.isValidPhoneNumber(locale))
+        assertFalse(input8.isValidPhoneNumber(locale))
+    }
+
+    @Test
+    fun `validate email address`() {
+        val input1 = "atta@atta.ai"
+        val input2 = "atta.ai"
+        val input3 = "atta.ai"
+        val input4 = "atta@yahoo.co.jp"
+        val input5 = "atta@gmail.com"
+        assertTrue(input1.isValidEmail())
+        assertFalse(input2.isValidEmail())
+        assertFalse(input3.isValidEmail())
+        assertTrue(input4.isValidEmail())
+        assertTrue(input5.isValidEmail())
+    }
+}
